5b7a7343a9df3c990623194dabdcf3def6fd2a71
Pamela Stockwell <pjstockw@j270-pjsmac.isd.glam.ac.uk>
2013-08-09 15:19:46 +0100

    Commiting the application folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '5adf8a71dd2a756d9634386630920f3126a9a2695a9083ace0eefe68b55f82b322f63601981df121f79a270de30593d0276dfd8113c1f70cfcd68537d828c2ed'
